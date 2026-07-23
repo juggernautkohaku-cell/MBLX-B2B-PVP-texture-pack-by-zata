ee3853b122da9c27083c4433831f11e677b5dded
ZataLove_JP_Kohaku <juggernautkohaku@gmail.com>
2026-07-23 14:35:54 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 const URL_MAP = {
   "||miniblox.*textures/spritesheet.png": "https://raw.githubusercontent.com/ZataLove-JP-Kohaku/UnverifiedV2-zata/refs/heads/main/spritesheet.png",
+  "||miniblox.*assets/default-DKNlYibk.png": "https://raw.githubusercontent.com/juggernautkohaku-cell/my-pvp-pack/refs/heads/main/DEBUG/default.png",
   "||miniblox.*assets/Miniblox-Regular-Lz-C8nWS.otf": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/assets/Miniblox-Regular-Lz-C8nWS.otf",
   "||miniblox.*textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/refs/heads/main/entity/chicken.png",
   "||miniblox.*textures/entity/cow/cow.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/refs/heads/main/entity/cow.png",
